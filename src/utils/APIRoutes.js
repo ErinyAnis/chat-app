e90fa0ed08db5ca9d16b3464e3a5dec5b55c82bf
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2025-02-06 21:59:07 +0200

    update

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,9 +1,12 @@
-export const host = import.meta.env.VITE_BACKEND_ROUTE_URL;
+// export const host = import.meta.env.VITE_BACKEND_ROUTE_URL;
 // export const host = "http://localhost:5000";
+export const host = import.meta.env.VITE_BACKEND_ROUTE_URL.replace(/\/$/, ""); // Remove trailing slash
+
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
 export const allUsersRoute = `${host}/api/auth/allusers`;
 export const sendMessageRoute= `${host}/api/messages/addmsg`;
 export const getAllMessagesRoute= `${host}/api/messages/getmsg`;
+
 
