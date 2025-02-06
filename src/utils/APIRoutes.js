08a4d6159da94c95f00efe936c0e53b68ebfdacb
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2025-02-06 21:02:40 +0200

    add .env file

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5000";
+export const host = import.meta.env.VITE_BACKEND_ROUTE_URL;
+// export const host = "http://localhost:5000";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
